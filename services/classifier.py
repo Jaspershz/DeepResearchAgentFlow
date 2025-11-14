9471b7e8b289735cfbce49c0e890f64639e5e1a8
Jasper S <jaspershu19@gmail.com>
2025-11-14 10:54:28 +0500

    deleted duplicates

--- services/classifier.py
+++
@@ -1,5 +0,0 @@
-def classify(text: str, demo_mode: bool = True) -> str:
-    if "research" not in text:
-        return "quick"
-    else:
-        return "research"
--- services/llm_client.py
+++
@@ -1,2 +0,0 @@
-def generate(user_query):
-    return 0
